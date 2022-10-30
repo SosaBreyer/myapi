9f8ba51038bdc5ed1017f0c71defb8dd840c027c
Javier Sosa Breyer <13318348+SosaBreyer@users.noreply.github.com>
2022-10-30 04:28:26 +0100

    api key authentication

---
+++ src/Controller/ApiLoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class ApiLoginController extends AbstractController
+{
+    /**
+     * @Route("/api/login", name="api_login")
+     */
+    public function index(): JsonResponse
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = $this->getUser();
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = 'apiToken1234'; // somehow create an API token for $user
+
+        $user->setApitoken($token);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json([
+            'user'  => $user->getUserIdentifier(),
+            'token' => $token,
+        ]);
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    /** @var UserPasswordHasherInterface */
+    private $hasher;
+
+    /**
+     * @param UserPasswordHasherInterface $encoder
+     */
+    public function __construct(UserPasswordHasherInterface	$hasher) {
+        $this->hasher = $hasher;
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('apiUser');
+        $user->setPassword($this->hasher->hashPassword(
+            $user,
+            'apiUserPassword'
+        ));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
